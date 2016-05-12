7869eee8579a37a72a79a60af9046f7a125b1df0
Michael Cebrian <michael.cebrian@gmail.com>
2016-05-12 03:31:32 -0400

    Add login service to login component

--- client/app/components/survey-login.ts
+++ client/app/components/survey-login.ts
@@ -1,9 +1,16 @@
 import {Component} from '@angular/core';
+import {Router} from '@angular/router-deprecated';
 import {MD_CARD_DIRECTIVES} from '@angular2-material/card';
 import {MdButton} from '@angular2-material/button';
 import {MD_LIST_DIRECTIVES} from '@angular2-material/list';
 import {MD_INPUT_DIRECTIVES} from '@angular2-material/input';
 import {MdIcon} from '@angular2-material/icon';
+import {SurveyLoginService} from '../services/survey-login.svc';
+
+interface IUser {
+  isLoggedIn: boolean;
+  type: string;
+}
 
 @Component({
   selector: 'survey-login',
@@ -17,14 +24,23 @@ import {MdIcon} from '@angular2-material/icon';
   ]
 })
 export class SurveyLogin {
+  router: Router;
   login: Object;
+  loginSvc: SurveyLoginService;
   selectedIndex: number = -1;
 
   submit(): void {
-    console.log('submitted');
+    this.loginSvc.login(this.login);
   }
 
-  constructor() {
+  constructor(router: Router, loginSvc: SurveyLoginService) {
+    this.router = router;
+    this.loginSvc = loginSvc;
+    this.loginSvc.login$.subscribe((res: IUser) => {
+      if (res.isLoggedIn) {
+        this.router.navigate(['List']);
+      }
+    });
     this.login = {
       user: '',
       pass: ''
